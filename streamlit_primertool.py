900b04b83e0a6f9b3259ee2f39a70028797fa51a
simon.michau <simon.michau@rwth-aachen.de>
2024-02-28 10:58:40 +0100

    Improved streamlit apps user experience

--- streamlit_primertool.py
+++ streamlit_primertool.py
@@ -1,7 +1,20 @@
 import streamlit as st
 from primertool import primertool as pt
 
+st.set_page_config(page_title="Primertool", page_icon="logo.png")  # Dna icons created by Freepik - Flaticon
+
 st.title("Primertool")
+st.markdown(f"This tool is built to facilitate the process of finding primers for non-technical users. "
+            f"Primers can be found for a given variant, exon, entire gene or genomic position.")
+st.markdown("To get started, please")
+st.markdown("- enter your initials (Kürzel); Default is None")
+st.markdown(f"- select the genome assembly you are working with (default is hg38)")
+st.markdown(f"- select the primer generation method that corresponds to the type of primer you are looking for and "
+            f"enter the required information")
+st.markdown(f'- hit the enter button and wait until the order table is generated (depending on your input '
+            f'this may take some time)')
+st.markdown(f"Note: When running the tool for the first time, it may take several minutes to download the selected "
+            f"reference genome. Subsequent runs will be faster.")
 
 st.sidebar.markdown("# Primertool")
 
@@ -9,7 +22,7 @@ st.markdown('---')
 
 kuerzel = st.text_input(label='', placeholder='Initials', help='Enter your initials (e.g. "XY")')
 genome_assembly = st.selectbox('Genome assembly', ('hg38', 'hg19'), index=0, help='Select genome assembly')
-generator = st.selectbox('Generate from:', ('Variant', 'Exon', 'Gene', 'Genomic Position'), index=0,
+generator = st.selectbox('Generate from:', ('Variant', 'Exon', 'Gene', 'Genomic Position'),
                          help='Select what input you want to use to generate primers')
 
 st.markdown('---')
@@ -21,10 +34,11 @@ if generator == 'Variant':
 
     if genome_assembly and hgvs_variant:
         if not kuerzel:
-            st.warning("Please enter your initials")
+            st.warning("Don't forget to enter your initials")
             kuerzel = None
-        df_ordertable = pt.VariantPrimerGenerator(hgvs_variant, genome_assembly, kuerzel=kuerzel).ordertable
-        st.markdown(f"Primers for variant {hgvs_variant} in genome assembly {genome_assembly} are:")
+        with st.spinner('Generating primers...'):
+            df_ordertable = pt.VariantPrimerGenerator(hgvs_variant, genome_assembly, kuerzel=kuerzel).ordertable
+        st.success(f"Primers for variant {hgvs_variant} in genome assembly {genome_assembly} are:")
         st.dataframe(df_ordertable.style.hide_index())
 elif generator == 'Exon':
     st.subheader("ExonPrimerGenerator")
@@ -33,10 +47,11 @@ elif generator == 'Exon':
 
     if genome_assembly and nm_number and exon_number:
         if not kuerzel:
-            st.warning("Please enter your initials")
+            st.warning("Don't forget to enter your initials")
             kuerzel = None
-        df_ordertable = pt.ExonPrimerGenerator(nm_number, exon_number, genome_assembly, kuerzel=kuerzel).ordertable
-        st.markdown(f"Primers for exon {exon_number} in gene {nm_number} in genome assembly {genome_assembly} are:")
+        with st.spinner('Generating primers...'):
+            df_ordertable = pt.ExonPrimerGenerator(nm_number, exon_number, genome_assembly, kuerzel=kuerzel).ordertable
+        st.success(f"Primers for exon {exon_number} in gene {nm_number} in genome assembly {genome_assembly} are:")
         st.dataframe(df_ordertable.style.hide_index())
 elif generator == 'Gene':
     st.subheader("GenePrimerGenerator")
@@ -44,10 +59,11 @@ elif generator == 'Gene':
 
     if genome_assembly and nm_number:
         if not kuerzel:
-            st.warning("Please enter your initials")
+            st.warning("Don't forget to enter your initials")
             kuerzel = None
-        df_ordertable = pt.GenePrimerGenerator(nm_number, genome_assembly, kuerzel=kuerzel).ordertable
-        st.markdown(f"Primers for gene {nm_number} in genome assembly {genome_assembly} are:")
+        with st.spinner('Generating primers...'):
+            df_ordertable = pt.GenePrimerGenerator(nm_number, genome_assembly, kuerzel=kuerzel).ordertable
+        st.success(f"Primers for gene {nm_number} in genome assembly {genome_assembly} are:")
         st.dataframe(df_ordertable.style.hide_index())
 elif generator == 'Genomic Position':
     st.subheader("GenomicPositionPrimerGenerator")
@@ -57,13 +73,18 @@ elif generator == 'Genomic Position':
 
     if genome_assembly and chromosome and start_position and end_position:
         if not kuerzel:
-            st.warning("Please enter your initials")
+            st.warning("Don't forget to enter your initials")
             kuerzel = None
-        df_ordertable = pt.GenomicPositionPrimerGenerator(chromosome, start_position, end_position, genome_assembly, kuerzel=kuerzel).ordertable
-        st.markdown(f"Primers for genomic position {chromosome}:{start_position}-{end_position} in genome assembly {genome_assembly} are:")
+        with st.spinner('Generating primers...'):
+            df_ordertable = pt.GenomicPositionPrimerGenerator(chromosome, start_position, end_position, genome_assembly, kuerzel=kuerzel).ordertable
+        st.success(f"Primers for genomic position {chromosome}:{start_position}-{end_position} in genome assembly {genome_assembly} are:")
         st.dataframe(df_ordertable.style.hide_index())
 
-#if button_clicked:
+
+# st.balloons()
+# st.snow()
+
+# if button_clicked:
 #    if not kuerzel:
 #        st.warning("Please enter your initials")
 #    if kuerzel and genome_assembly and hgvs_variant:
